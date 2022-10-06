fc6c1778a46bbb8ede1193727441bae2afaf84cc
MakimaGorky <114951816+MakimaGorky@users.noreply.github.com>
2022-10-06 13:15:51 +0300

    Create days in years.pas

---
+++ days in years.pas
@@ -0,0 +1,15 @@
+﻿begin
+  
+  var (y1,y2):=ReadInteger2();
+  
+  var c:=0;
+  
+  for var i:=y1 to y2 do
+    if ((i mod 4=0)and not(i mod 100=0))or(i mod 400=0) then
+      c+=366
+    else
+      c+=365;
+  
+  Println(c);
+  
+end.
